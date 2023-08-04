41fb1fb1dade19672bd026e35bfb7f42cc432e1d
GODIMONGO <106483765+GODIMONGO@users.noreply.github.com>
2023-08-05 01:30:22 +0300

    Add files via upload

--- yt_monster_py.py
+++ yt_monster_py.py
@@ -1,193 +1,257 @@
 import requests
-import speedtest
 import json
-import time
-from datetime import datetime
-token_task = ''
-token_work = ''
-id_task = ''
+import base64
 
-# Версия библиотеки 2.3
-def Version():
-    return '2.3'
-def log(text): #Логирование в фаел лога
-    with open('log.txt', 'a') as f:
-        f.write('\n' + str(datetime.now()) + '  ' + text)
-        f.close()
+url_clifl = "https://api.clifl.com/"
 
-def ytmonster_error(error):
-    errors = {
-        900: 'Лимит, возникает при большом числе запросов.',
-        901: 'Лимит, возникает при большом числе ошибок связанных с токеном (1001-1004)',
-        902: 'Неверный тип токена. Например: вы используете Ключ доступа (для выполнения заданий) при добавлении задания',
-        1001: 'Отсутвует токен',
-        1002: 'Не найден токен',
-        1003: 'Токен отключен Включите токен на сайте: https://ytmonster.ru/api/#key',
-        1004: 'Ошибка токена',
-        1101: 'Неправильный тип задания add-account',
-        1102: 'Ошибка в ссылке аккаунта',
-        1103: 'Ошибка в ссылке аккаунта',
-        1301: 'Неправильный параметр get-accounts',
-        1401: 'Не найден аккаунт untie',
-        1501: 'Не найден аккаунт id_account',
-        1502: 'Не найден тип задания по get-task',
-        1503: 'Нет заданий данного типа',
-        1504: 'timeout - перерыв необходимый для получения нового задания выбранного типа',
-        1601: 'Не найден аккаунт id_account',
-        1602: 'Задание не выполнено',
-        1603: 'Задание было выполнено ранее',
-        1701: 'Не найден аккаунт id_account',
-        1109: 'Ошибка, проверьте поле error_response',
-        1509: 'Ошибка, проверьте поле error_response',
-        1609: 'Ошибка, проверьте поле error_response',
-        1809: 'Ошибка, проверьте поле error_response',
-        1909: 'Ошибка, проверьте поле error_response',
-        2009: 'Ошибка, проверьте поле error_response',
-        3009: 'Ошибка, проверьте поле error_response'
+
+def version():
+    return 3.0
+
+
+def balance_coin(token):
+    global url_clifl
+
+    params = {
+        "action": "user-balance",
+        "token": str(token)
     }
 
-    error = int(error)
-    if error in errors:
-        return '', errors[error]
+    response = requests.post(url_clifl, data=params).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    return response['response']['coin'], 'NO'
+
+def balance_many(token):
+    global url_clifl
+
+    params = {
+        "action": "user-balance",
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=params).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    return int(response['response']['money']), 'NO'
+
+def add_account(token, platform, url):
+    global url_clifl
+
+    params = {
+        "action": "accounts-add",
+        "platform": str(platform),
+        "url": str(url),
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=params).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    response = {'account': str(response['response']['account']), 'task': str(response['response']['task']),
+                'url': str(response['response']['url']), 'type': str(response['response']['type'])}
+    return response, 'NO'
+
+def check_account(token, account, task):
+    global url_clifl
+
+    params = {
+        "action": "accounts-check",
+        "account": str(account),
+        "task": str(task),
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=params).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    return str(response['status']), 'NO'
+
+
+import requests
+
+def get_task_list(token, platform, offset=0, limit=100):
+    global url_clifl
+    data = {
+        "action": "mytasks-get",
+        "platform": str(platform),
+        "offset": str(offset),
+        "limit": str(limit),
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=data).json()
+
+    if response['status'] != 'success':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response.get('error')
+    elif not response['response']:
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', 'Нет заданий данного типа'
     else:
-        return '', 'ok'
+        task_list = response['response']
+        processed_tasks = []
 
-
-def ytmonster_req(token, task, id='', soc_name= '', type= ''): #запрос к ytmonster и обработка некоторых ошибок
-    token_work = token[0]
-    token_task = token[1]
-    print('ping: ytmonster.ru ...')
-    try:
-        if task == 'balance':
-            req = requests.get('https://app.ytmonster.ru/api/?balance=get&token=' + token_work)
-            json1 = json.loads(req.text)
-            a, err = ytmonster_error(json1["error"])
-            if err != 'ok':
-                return '', err
+        for task in task_list:
+            processed_task = {}
+            processed_task['id'] = int(task['id'])
+            processed_task['platform'] = str(task['platform'])
+            if task['type'] == 'viewg':
+                processed_task['type'] = 'view'
             else:
-                return json1["response"]["balance"], 'ok'
-        elif task == 'close_client': #закрытие клиента
-            req = requests.get('https://app.ytmonster.ru/api/?close-client='+ id +'&token='+ token_work)
-            json1 = json.loads(req.text)
-            a, err = ytmonster_error(json1["error"])
-            if err != 'ok':
-                return '', err
+                processed_task['type'] = str(task['type'])
+            processed_task['short_url'] = str(task['short_url'])
+            processed_task['url'] = str(task['url'])
+            if task['valh'] == '&infin;':
+                processed_task['valh'] = 0
             else:
-                return 'клиент закрыт', 'ok'
-            print('ok')
-        elif task == 'get_client': #получение работающих клиентов
-            req = requests.get('https://app.ytmonster.ru/api/?get-clients=get&token=' + token_work)
-            ID_CLIENT = []
-            json1 = json.loads(req.text)
-            a, err = ytmonster_error(json1["error"])
-            if err != 'ok':
-                return a, err
-            if req.text == """{"error":0,"response":[]}""":
-                return 'not_work', 'Нет рабочих клиентов', ID_CLIENT
-            b = 0
-            mess = '--------------\n'
-            while len(json1['response']) > b:
-                mess = mess + '\n👤 Номер клиента: ' + str(b) + '\n🆔 ID клиента: ' + str(json1["response"][b]["id"]) + \
-                       '\n🌐 Тип браузера: ' + str(
-                    json1["response"][b]["type_browse"]["name"]) + '\n⏳ Осталось просмотреть: ' + str(
-                    json1["response"][b]["info"]["sec"]) + ' сек.' + \
-                       '\n📺 Ссылка на просмотриваемое видео: https://www.youtube.com/watch?v=' + \
-                       json1["response"][b]["info"]["http"] + \
-                       '\n❌ Количество ошибок при просмотре: ' + str(json1["response"][b]["info"]["error"]) + \
-                       '\n🌐 IP клиента: ' + str(json1["response"][b]["info"]["ip"]) + \
-                       '\n🔴 Статус аккаунта ютуб:' + str(json1["response"][b]["accounts"]["youtube"]) + \
-                       '\n👀 Просмотрел видео: ' + str(json1["response"][b]["data"]["count"]) + \
-                       '\n💰 Заработал за просмотр видео: ' + str(json1["response"][b]["data"]["coin"]) + ' COIN' + \
-                       '\n✅ Выполнил заданий: ' + str(json1["response"][b]["data"]["count_task"]) + \
-                       '\n💰 Заработал за выполнения заданий: ' + str(json1["response"][b]["data"]["coin_task"]) + '\n--------------'
-                ID_CLIENT.append(json1["response"][b]["id"])
-                b = b + 1
-            return mess, err, ID_CLIENT
+                processed_task['valh'] = int(task['valh'])
+            processed_task['need'] = int(task['need'])
+            processed_task['now'] = int(task['now'])
+            processed_tasks.append(processed_task)
+
+        return processed_tasks, 'NO'
 
 
-        elif task == 'my_task': # получения списка заданий
-            req = requests.get('https://app.ytmonster.ru/api/?my-tasks=' + id +'&offset=0&token=' + token_task)
-            json1 = json.loads(req.text)
-            a, err = ytmonster_error(json1["error"])
-            if '''{"error":0,"response":[]}''' == req.text:
-                return 'Нет заданий такого типа!', err
-            if err != 'ok':
-                return '', err
-            i = len(json1['response'])
-            b = 0
-            mess = ''
-            while i > b:
-                time.sleep(1)
-                mess = mess + '\n'+ 'Нужно выполнить: ' + json1["response"][b]["need"] + '/Выполнено: '+ json1["response"][b]["now"] +'\nКоличество выполнения в час: '+ json1["response"][b]["valh"] +'\nID: ' + json1["response"][b]["id"] + '\n' + 'Сылка: ' + json1["response"][b]["url"] + '\n' + 'Тип: ' + json1["response"][b]["soc"] + '\n-----------'
-                b = b + 1
-            return mess, err
+def add_task(token, platform, href, count, coin, valh=0, sec=None, comments=None, sec_max=None, params=None, type=None):
+    global url_clifl
+    from urllib.parse import parse_qs, urlparse
+    task = {"action": "mytasks-add", "token": str(token), "platform": str(platform), "count": str(count), "valh": str(valh)}
+    if platform == 'tg' and type == 'view':
+        task["coin"] = str(100)
+    else:
+        task["coin"] = str(coin)
 
-        elif task == 'remove_task':
-            req = requests.get('https://app.ytmonster.ru/api/?edit-task=remove&id_task=' + id + '&soc_name=' + soc_name + '&type=' + type + '&token=' + token_work)
-            json1 = json.loads(req.text)
-            a, err = ytmonster_error(json1["error"])
-            if err != 'ok':
-                return '', err
-            else:
-                mess = json1['response']["status"]
-                print(mess)
 
+    if type != None:
+        task["type"] = str(type)
+    if platform == "ytview" or platform == "ytlike" or platform == "ytcomm":
+        if "/shorts/" in href:
+            parsed_url = urlparse(href)
+            path = parsed_url.path
+            parts = path.split('/')
+            href = parts[-1]
         else:
-            log('Ошибка выполнения функции ytmonster! Нет указания что делать')
-            return '', 'Ошибка выполнения функции ytmonster! Нет указания что делать'
+            parsed_url = urlparse(href)
+            video_id = parsed_url.path.lstrip("/")
+            href = f"https://www.youtube.com/watch?v={video_id}"
 
-    except requests.exceptions.RequestException:
-        time.sleep(10)
-        print('ping err')
-        err = 'Ошибка запроса к API ytmonster! Работает ли сайт?'
-        return '', err
-    return req, 'ok'
+    if platform == "inst":
+        from urllib.parse import urlparse, urlunparse
+        parsed_url = urlparse(href.encode('utf-8'))
+        netloc_parts = parsed_url.netloc.split(b'.')
+        if len(netloc_parts) == 2 and netloc_parts[0] == b'instagram':
+            netloc_parts.insert(0, b'www')
+            new_netloc = b'.'.join(netloc_parts)
+            new_parsed_url = parsed_url._replace(netloc=new_netloc)
+            href = urlunparse(new_parsed_url).decode('utf-8')
 
-def test_speed(times): #сколько раз замерить скорость интернета
-    st = speedtest.Speedtest()
-    download_speeds = []
-    upload_speeds = []
-    for i in range(times):
-        download_speed = st.download() / 1000000  # скорость загрузки в Мбит/с
-        upload_speed = st.upload() / 1000000  # скорость отгрузки в Мбит/с
-        download_speeds.append(download_speed)
-        upload_speeds.append(upload_speed)
-        print(f"Скорость загрузки {i + 1}: {download_speed:.2f} Мбит/с")
-        print(f"Скорость отгрузки {i + 1}: {upload_speed:.2f} Мбит/с")
-    avg_download_speed = sum(download_speeds) / len(download_speeds)
-    avg_upload_speed = sum(upload_speeds) / len(upload_speeds)
-    return f"Средняя скорость загрузки: {avg_download_speed:.2f} Мбит/с" + '\n' + f"Средняя скорость отгрузки: {avg_upload_speed:.2f} Мбит/с"
 
-# Beta функция на данный момент не доработана
+    href = base64.b64encode(href.encode('utf-8'))
+    href = href.decode('utf-8')
+    task["href"] = str(href)
+    if platform == "ytview":
+        task["sec"] = str(sec)
+    if platform == "ytcomm":
+        comments = base64.b64encode(comments.encode('utf-8'))
+        comments = comments.decode('utf-8')
+        task["comments"] = str(comments)
+    if platform == 'ytview' and sec_max != None:
+        task["sec_max"] = sec_max
+    if platform == 'tg' and type == 'like':
+        reaction_dict = {}
+        reactions_list = params.split(',')
 
-def yt_monster_create_task_tg(task, task_href, token, reactions='', task_count=10, task_valh=0, task_coin= 0):
-    token_work = token[0]
-    token_task = token[1]
-    if task == 'like_tg':
-        if reactions == '':
-            return '', 'err'
-        import json
-        import base64
-        task_reactions = base64.b64encode(json.dumps({"reactions": reactions}).encode()).decode()
-        print(task_reactions)
-        print(str(base64.b64encode(task_href.encode()).decode()))
-        url = 'https://app.ytmonster.ru/api/?add-task=tg&href=' + str(base64.b64encode(task_href.encode()).decode()) + '&count=' + str(task_count) + '&type=like&valh=' + str(task_valh) + '&token=' + token_task + '&params=' + task_reactions
-        req = requests.get(url)
-        json1 = json.loads(req.text)
-        a, err = ytmonster_error(json1["error"])
-        if err != 'ok':
-            return json1["error_response"], err
-        req = 'Статус: ' + str(json1["response"]["status"]) + '\nID: ' + str(json1["response"]["id"])
-        return req, 'ok'
-    elif task == 'view_tg':
-        import json
-        import base64
-        print(str(base64.b64encode(task_href.encode()).decode()))
-        url = 'https://app.ytmonster.ru/api/?add-task=tg&href=' + str(base64.b64encode(task_href.encode()).decode()) + '&count=' + str(task_count) + '&type=view&valh=' + str(task_valh) + '&coin=' + str(task_coin) + '&token=' + token_task
-        req = requests.get(url)
-        json1 = json.loads(req.text)
-        a, err = ytmonster_error(json1["error"])
-        if err != 'ok':
-            return json1["error_response"], err
-        req = 'Статус: ' + str(json1["response"]["status"]) + '\nID: ' + str(json1["response"]["id"])
-        return req, 'ok'
+        for reaction in reactions_list:
+            reaction_name, reaction_percentage = reaction.split(':')
+            reaction_dict[reaction_name] = int(reaction_percentage)
+
+        encoded_data = base64.b64encode(json.dumps({"reactions": reaction_dict}).encode('utf-8'))
+        params = encoded_data.decode('utf-8')
+    if platform == 'tg' and type == 'poll':
+        answers_dict = {}
+        answers_list = params.split(',')
+
+        for answer in answers_list:
+            answer_index, answer_percentage = answer.split(':')
+            answers_dict[int(answer_index)] = int(answer_percentage)
+
+        encoded_data = base64.b64encode(json.dumps({"poll": answers_dict}).encode('utf-8'))
+        params = encoded_data.decode('utf-8')
+    if params != None:
+        task["params"] = str(params)
+    print(task)
+
+    response = requests.post(url_clifl, data=task).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    return response['id'], 'NO'
+
+def task_remove(token, platform,  id):
+    global url_clifl
+
+    params = {
+        "action": "mytasks-remove",
+        "platform": str(platform),
+        "id": str(id),
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=params).json()
+
+    if response['status'] != 'success' and response['error'] != '':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response['error']
+    return str(response['status']), 'NO'
+
+
+def task_addition(token, platform, id, count):
+    global url_clifl
+    data = {
+        "action": "mytasks-addition",
+        "id": str(id),
+        "platform": str(platform),
+        "count": str(count),
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=data).json()
+
+    if response['status'] != 'success':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response.get('error')
+    else:
+        return str(response['status']), 'NO'
+
+def ytclients_get(token):
+    global url_clifl
+    data = {
+        "action": "ytclients-get",
+        "token": str(token)
+    }
+
+    response = requests.post(url_clifl, data=data).json()
+    if response['status'] != 'success':
+        return 'Возникла ошибка. Пожалуйста, проверьте второе значение!', response.get('error')
+    else:
+        task_list = response['response']
+        all_processed_tasks = []
+
+        for client in task_list:
+            processed_task = {
+                'id': str(client['id']),
+                'sec': int(client['info']['sec']),
+                'http': client['info']['http'],
+                'error': int(client['info']['error']),
+                'ip': client['info']['ip'],
+                'youtube_account': client['accounts']['youtube'],
+                'coin': int(client['data']['coin']),
+                'coin_task': int(client['data']['coin_task']),
+                'count': int(client['data']['count']),
+                'count_task': int(client['data']['count_task'])
+            }
+            all_processed_tasks.append(processed_task)
+
+        return all_processed_tasks
